1d3fc3ab723b7daa5b1fe7ac396bd4e7366741ce
Pereyra-Diego <diegora728@gmail.com>
2024-05-25 18:31:40 -0300

    get y set de bodega

--- Clases/Bodega.java
+++ Clases/Bodega.java
@@ -13,5 +13,41 @@ public class Bodega {
     public void contarResena(){}
     public void mostrarTodosVinos(){}
     public void getBodega(){}
+    public String[] getCoordenadasUbicacion() {
+        return coordenadasUbicacion;
+    }
+    public void setCoordenadasUbicacion(String[] coordenadasUbicacion) {
+        this.coordenadasUbicacion = coordenadasUbicacion;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public String getHistoria() {
+        return historia;
+    }
+    public void setHistoria(String historia) {
+        this.historia = historia;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Date getPeriodoActualizacion() {
+        return periodoActualizacion;
+    }
+    public void setPeriodoActualizacion(Date periodoActualizacion) {
+        this.periodoActualizacion = periodoActualizacion;
+    }
+    public RegionVitivinicola getRegionVitivinicola() {
+        return regionVitivinicola;
+    }
+    public void setRegionVitivinicola(RegionVitivinicola regionVitivinicola) {
+        this.regionVitivinicola = regionVitivinicola;
+    }
 }
 
